32db0a52fd47f770f6043e0d8b476f1816045aac
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-14 16:55:13 +0530

    Add project files.

---
+++ EmpProject/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpProject
+{
+    public class Employee
+    {
+        int empid;
+        string empname;
+        float sal, allow;
+        public Employee()
+        {
+            empid = 1;
+            empname = "xyz";
+            sal = 10000;
+            allow = 500;
+        }
+
+        public Employee(int empid, string empname, float sal, float allow)
+        {
+            this.empid = empid;
+            this.empname = empname;
+            this.sal = sal;
+            this.allow = allow;
+        }
+        public string CalculateSalary()
+
+        {
+            return $"Salary:{sal + allow}";
+
+        }
+        public string PrintValue()
+        {
+            return $"Employee Detail: {empid}/{empname}/{sal}/{allow}";
+        }
+    }
+}
---
+++ EmpProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            Console.WriteLine(employee.PrintValue());
+          Employee employee2 = new Employee(1,"xyz",10000,500);
+            Console.WriteLine(employee2.PrintValue());
+        }
+    }
+}
